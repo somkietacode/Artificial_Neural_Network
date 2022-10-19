7e7c17c19ae9caf7f145b1944597d8192d5990c9
somkietacode <108131461+somkietacode@users.noreply.github.com>
2022-10-19 18:48:11 +0000

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='artificial_neural_network',
+    version='1.0.0',
+    install_requires=[
+        'numpy',
+        'importlib-metadata; python_version == "3.8"',
+    ],
+)
